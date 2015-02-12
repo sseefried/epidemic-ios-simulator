d799ef89069befced528e57e44c549b8c58cc4f5
Sean Seefried <sean.seefried@gmail.com>
2015-02-12 14:51:02 +1100

    Update for changed epidemic lib.

--- main.c
+++ main.c
@@ -2,16 +2,13 @@
 #include "SDL.h"
 #include <time.h>
 
-#define SCREEN_WIDTH 320
-#define SCREEN_HEIGHT 400
-
 extern void haskell_main(void);
-extern void __stginit_MainLib (void);
+extern void __stginit_IOSMain (void);
 
 int main(int argc, char *argv[])
 {
   hs_init(&argc, &argv);
-  hs_add_root(__stginit_MainLib);
+  hs_add_root(__stginit_IOSMain);
   haskell_main();
   hs_exit();
   return 0;
