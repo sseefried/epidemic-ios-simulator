ef29ae3e8de055e02d6336c275929367a20c6f6a
Sean Seefried <sean.seefried@gmail.com>
2015-03-06 15:29:05 +1100

    Added aarch64 files and updated mk-fat-sh script

--- lib/mk-fat.sh
+++ lib/mk-fat.sh
@@ -8,7 +8,13 @@ for i in libEpidemic.a libSDL2.a libSDL2_mixer.a \
          libcairo.a libfreetype.a libpixman-1.a libpng.a\
          libogg.a libvorbis.a libvorbisfile.a libz.a; do
 
-  CMD="lipo i386/$i armv7/$i -create -output $i"
+  for arch in i386 armv7 aarch64; do 
+    if [ ! -f $arch/$i ]; then
+      echo "Could not find $arch/$i!"
+    fi
+  done
+
+  CMD="lipo i386/$i armv7/$i aarch64/$i -create -output $i"
   echo $CMD
   $CMD
 
