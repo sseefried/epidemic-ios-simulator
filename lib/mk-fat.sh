9a155d02c72e036164235e378e1632c9e93f9f2e
Sean Seefried <sean.seefried@gmail.com>
2015-03-31 17:05:52 +1000

    Now use UPX to reduce the size of the executable *before* code signing.
    This massively reduces the size of the final .ipa archive from 46.7MB to 8.5MB

--- lib/mk-fat.sh
+++ lib/mk-fat.sh
@@ -8,13 +8,16 @@ for i in libEpidemic.a libSDL2.a libSDL2_mixer.a \
          libcairo.a libfreetype.a libpixman-1.a libpng.a\
          libogg.a libvorbis.a libvorbisfile.a libz.a; do
 
+  SUBCMD=""
   for arch in i386 armv7 aarch64; do 
     if [ ! -f $arch/$i ]; then
       echo "Could not find $arch/$i!"
     fi
+    SUBCMD="$SUBCMD $arch/$i"
   done
 
-  CMD="lipo i386/$i armv7/$i aarch64/$i -create -output $i"
+
+  CMD="lipo $SUBCMD -create -output $i"
   echo $CMD
   $CMD
 
